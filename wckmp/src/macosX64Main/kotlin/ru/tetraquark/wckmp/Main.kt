8cb2898264a392c9a1de98338a41a799e4837596
tetraquark <avladislavv@gmail.com>
2020-07-26 22:26:24 +0700

    small cleanup and renames

--- wckmp/src/macosX64Main/kotlin/ru/tetraquark/wckmp/Main.kt
+++
@@ -1,9 +0,0 @@
-package ru.tetraquark.wckmp
-
-fun main(args: Array<String>) {
-    val resultList = multithreadLinesCounter(args.asList())
-
-    println("------------------------------------")
-
-    resultList.forEach(::printFileLinesCount)
-}
